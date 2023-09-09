c03997cc6675b9483005db0b45960337a309d01f
Chikmurgechetan <chikmurgechetan@gmail.com>
2023-09-09 15:34:19 +0530

    Fixx deleting Code

--- components/Completed.js
+++ components/Completed.js
@@ -5,7 +5,7 @@ import classes from "./Completed.module.css";
 function Completed() {
   const [todo, setTodo] = useState("");
   const [todoList, setTodoList] = useState([]);
-  const [editingItemId, setEditingItemId] = useState(null);
+
   const router = useRouter();
 
   const fetchData = async () => {
@@ -28,43 +28,23 @@ function Completed() {
 
   const submitHandler = async (event) => {
     event.preventDefault();
-    if (!todo.trim()) {
-      // Validate that the todo is not empty
-      return;
-    }
 
     const newTask = { todo, isCompleted: false };
 
     try {
-      if (editingItemId) {
-        const response = await fetch(`/api/${editingItemId}`, {
-          method: "PUT",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(newTask),
-        });
-        if (response.ok) {
-          console.log("Item updated successfully");
-          setEditingItemId(null);
-        } else {
-          console.error("Error updating item:", response.status);
-        }
-      } else {
-        const response = await fetch("/api", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(newTask),
-        });
+      const response = await fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newTask),
+      });
 
-        if (response.ok) {
-          fetchData();
-          setTodo("");
-        } else {
-          console.error("Error adding item:", response.status);
-        }
+      if (response.ok) {
+        fetchData();
+        setTodo("");
+      } else {
+        console.error("Error adding item:", response.status);
       }
     } catch (error) {
       console.error("Error:", error.message);
@@ -76,21 +56,20 @@ function Completed() {
   };
 
   const handleCheckboxChange = async (id) => {
-   
-      const updatedData = todoList.map((item) => {
-        if (item._id === id) {
-          // Toggle the isCompleted value
-          console.log(item);
-          return {
-            ...item,
-            isCompleted: !item.isCompleted,
-          };
-        }
-        return item;
-      });
-  
-      setTodoList(updatedData);
-         
+    const updatedData = todoList.map((item) => {
+      if (item._id === id) {
+        // Toggle the isCompleted value
+        console.log(item);
+        return {
+          ...item,
+          isCompleted: !item.isCompleted,
+        };
+      }
+      return item;
+    });
+
+    setTodoList(updatedData);
+
     const updatedItem = updatedData.find((item) => item._id === id);
     const response = await fetch(`/api/${id}`, {
       method: "PUT",
@@ -99,36 +78,28 @@ function Completed() {
       },
       body: JSON.stringify(updatedItem),
     });
-    
+
     console.log(response);
     const responseData = await response.text(); // Try using response.text() instead of response.json()
     console.log(responseData);
-    
-   
   };
-  
-  
-  
-  
+
   const handleDeleteItem = async (id) => {
-   try{
-    const response = await fetch (`/api/${id}`,{
-    method:'DELETE',
-   });
+    try {
+      const response = await fetch(`/api/${id}`, {
+        method: "DELETE",
+      });
 
-   if(response.ok){
-    fetchData()
-   console.log("item deleted successfully");
-  } else {
-    console.log("error deleting item:", response.status);
- }
-} catch (error) {
-  console.log("Error deleting item", error. message);
-}
-    
+      if (response.ok) {
+        fetchData();
+        console.log("item deleted successfully");
+      } else {
+        console.log("error deleting item:", response.status);
+      }
+    } catch (error) {
+      console.log("Error deleting item", error.message);
+    }
   };
-
-
 
   const navigateHandler = () => {
     router.push("/completed");
